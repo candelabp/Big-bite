f5061c02581acb25e52315efe7aeb3b24940c1da
Diego Canales <diegocanales112@gmail.com>
2024-09-13 01:21:51 -0300

    Sprint 1 vFinal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
+const cerrarMenu = document.getElementById("menu-icon-cerrar")
 
 
 menuIcon.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+cerrarMenu.addEventListener("click", () => {
+  navLinks.classList.remove('active');
+})
+
+
