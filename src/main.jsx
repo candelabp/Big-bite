7a9ed63d66e53495564806132b360d19ba5c28da
Candela Puerta <109047221+candelabp@users.noreply.github.com>
2024-09-23 12:48:28 -0300

    algunos arreglos para mi

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,17 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/js/src/carousel';
 import './styles.css';
 import './normalize.css';
-import { Menu } from './bigBite/pages/Menu';
+//import { Menu } from './bigBite/pages/Menu';
 import {Formulario} from './bigBite/pages/Formulario';
-import { Home } from './bigBite/pages/Home'
-import { InicioSesion } from './bigBite/pages/InicioSesion';
-
+//import { Home } from './bigBite/pages/Home'
+//import { InicioSesion } from './bigBite/pages/InicioSesion';
+//import { Login } from './bigBite/pages/Login'
 
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Formulario />
   </StrictMode>,
 )
