1c754564d042c68bf6b928f566432633211aab88
Candela Puerta <109047221+candelabp@users.noreply.github.com>
2024-09-25 19:33:02 -0300

    diego

--- src/bigBite/pages/Home.jsx
+++ src/bigBite/pages/Home.jsx
@@ -1,37 +1,61 @@
-
-import { CarouselComponent } from "../components/CarouselComponent"
-import { Footer } from "../components/Footer"
-import { NavBar } from "../components/NavBarRojo"
+import { CarouselComponent } from "../components/CarouselComponent";
+import { Footer } from "../components/Footer";
+import { NavBar } from "../components/NavBarRojo";
 import '../css/home.css';
-
+import { motion } from "framer-motion";
 
 export const Home = () => {
-    return (
-        <>
+  return (
+    <>
+      {/* navbar */}
+      <NavBar />
 
-            {/* navbar */}
+      <section className="content">
+        {/* Animación del título principal */}
+        <motion.h1
+          className="tituloBlanco"
+          initial={{ opacity: 0, y: -50 }} // Empieza desplazado hacia arriba e invisible
+          animate={{ opacity: 1, y: 0 }}   // Aparece y se mueve a su posición original
+          transition={{ duration: 2 }}     // Duración de 1 segundo
+        >
+          Gran <span className="separador">Sabor,</span>
+          <motion.span
+            className="tituloDestacado"
+            initial={{ scale: 0 }}           // Empieza con tamaño 0
+            animate={{ scale: 1 }}           // Crece hasta su tamaño original
+            transition={{ duration: 1, delay: 1 }} // Inicia después de 1 segundo
+          >
+            Big Bite.
+          </motion.span>
+        </motion.h1>
 
-            <NavBar />
+        <div className="container-ppal">
+          {/* Animación de la imagen */}
+          <motion.img
+            className="imgPrincipal"
+            src="src\\bigBite\\assets\\burguerInicio2.png"
+            initial={{ opacity: 0, x: -100 }}  // Empieza desplazada a la izquierda e invisible
+            animate={{ opacity: 1, x: 0 }}     // Aparece y se desplaza a su posición original
+            transition={{ duration: 1 }}       // Duración de 1 segundo
+          />
+        </div>
 
-            <section className="content">
+        {/* Animación del segundo título */}
+        <motion.h1
+          className="tituloAmarillo"
+          initial={{ opacity: 0, scale: 0.8 }}  // Comienza con una escala pequeña
+          animate={{ opacity: 1, scale: 1 }}    // Se expande a su tamaño original
+          transition={{ duration: 1, delay: 0.5 }}  // Aparece después de 0.5 segundos
+        >
+          Big Bite.
+        </motion.h1>
+      </section>
 
-                <h1 className="tituloBlanco">Gran <span className="separador">Sabor,</span><span className="tituloDestacado">Big Bite.</span></h1>
+      {/* carousel */}
+      <CarouselComponent />
 
-                <div className="container-ppal">
-                    <img className="imgPrincipal" src="src\bigBite\assets\burguerInicio2.png" />
-                </div>
-
-                <h1 className="tituloAmarillo ">Big Bite.</h1>
-            </section>
-
-            {/* carousel  */}
-
-            <CarouselComponent />
-
-            {/* footer */}
-
-            <Footer />
-            
-        </>
-    )
-}
+      {/* footer */}
+      <Footer />
+    </>
+  );
+};
