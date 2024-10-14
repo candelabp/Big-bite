5a520d3851494e7c2353c3c13381a79d34842e55
flopigimenez <fgimenezmorcos@gmail.com>
2024-10-14 11:58:56 -0300

    Create GestionProductos.jsx

---
+++ src/bigBite/pages/GestionProductos.jsx
@@ -0,0 +1,11 @@
+export const GestionProductos = () => {
+    return(
+    <>
+        <h1>Gestion de Productos</h1>
+        <p>Mira y actualiza el estado de los pedidos de hamburguesas</p>
+        <input type="text" />
+        <button>Exportar pedidos</button>
+        <button>Filtrar</button>
+    </>
+    )
+}
