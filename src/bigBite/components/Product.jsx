a05b16b8a7b6a0792cedaebcc520de0d4c73b4d1
candelabp <109047221+candelabp@users.noreply.github.com>
2024-10-23 12:30:11 -0300

    modal

--- src/bigBite/components/Product.jsx
+++ src/bigBite/components/Product.jsx
@@ -1,41 +1,56 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { dataContext } from './Context/DataContext';
 import '../css/menu.css';
 import ShoppingCart from '../assets/shopping-cart.png';
 
 const Product = () => {
     const { data, agregarCarrito } = useContext(dataContext);
-    // const { agregarCarrito } = useContext(dataContext);
-    // const [data, setData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null);
 
-    // useEffect(() => {
-    //     const fetchProducts = async () => {
-    //         try {
-    //             const response = await fetch('http://localhost:8080/productos');
-    //             const products = await response.json();
-    //             setData(products);
-    //         } catch (error) {
-    //             console.error('Error fetching products:', error);
-    //         }
-    //     };
+    const openModal = (product) => {
+        setSelectedProduct(product);
+        setShowModal(true);
+    };
 
-    //     fetchProducts();
-    // }, []); // El array vacío asegura que se ejecute solo una vez al montar el componente.
+    const closeModal = () => {
+        setSelectedProduct(null);
+        setShowModal(false);
+    };
 
     return (
-        <div className="product-grid"> {/* Cambié de "product-list" a "product-grid" */}
+        <div className="product-grid">
             {data.map((product) => (
                 <div className="product-card" key={product.id}>
                     <h3>{product.nombre}</h3>
-                    <img src={product.urlImagen} alt={product.nombre} />
+                    <img 
+                        src={product.urlImagen} 
+                        alt={product.nombre} 
+                        onClick={() => openModal(product)} 
+                        style={{ cursor: 'pointer' }} 
+                    />
                     <h3 className='precio-card'>${product.precio}</h3>
                     <button className="cart-button" onClick={() => agregarCarrito(product)}>
                         <img src={ShoppingCart} alt="Agregar al carrito" />
                     </button>
                 </div>
             ))}
+
+            {showModal && selectedProduct && (
+                <div className="modal-menu">
+                    <div className="modal-content">
+                        <span className="close-button" onClick={closeModal}>&times;</span>
+                        <h2>{selectedProduct.nombre}</h2>
+                        <img src={selectedProduct.urlImagen} alt={selectedProduct.nombre} />
+                        <p>Descripción: {selectedProduct.descripcion}</p>
+                        <h3>Precio: ${selectedProduct.precio}</h3>
+                    </div>
+                    <div className="modal-overlay" onClick={closeModal}></div>
+                </div>
+            )}
         </div>
     );
 };
 
 export default Product;
+
