5919f78f77cd3a61784daf98438ed3277a65d544
Alvaro Rubina <alvarorubina132@gmail.com>
2024-11-03 02:33:36 -0300

    Configurado el modal para ver los detalles de los pedidos

--- src/bigBite/components/modalVerDetalles.jsx
+++ src/bigBite/components/modalVerDetalles.jsx
@@ -100,24 +100,32 @@ const ModalVerDetalles = ({ onClose }) => {
 
 
             {isDetailsModalOpen && pedidos.map((pedido) => (
-                <div key={pedido.id} className="container-modal-detalles" id="modalDetails">
-                    <div className="div-modal">
-                        <div className="container-titulo-modal">
-                            <h1>Detalles del pedido #{pedido.id}</h1>
-                        </div>
-                        <div className='div-detalles'>
-                            <p><b>Producto:</b> {pedido.producto}</p>
-                            <p><b>Cantidad:</b> {pedido.cantidad}</p>
-                            <p><b>Precio:</b> ${pedido.precio}</p>
-                            <p><b>Total:</b> ${pedido.total}</p>
-                        </div>
-                        <div className="container-buttons-modal-detalles">
-                            <button className="button-modal cancel" onClick={closeDetailsModal}>Cerrar</button>
-                        </div>
+    <div key={pedido.id} className="container-modal-detalles" id="modalDetails">
+        <div className="div-modal">
+            <div className="container-titulo-modal">
+                <h1>Detalles del pedido #{pedido.id}</h1>
+            </div>
+            <div className='div-detalles'>
+                <p><b>Numero de orden:</b> {pedido.id}</p>
+                <p><b>Fecha del pedido:</b> {pedido.fechaSolicitado}</p>
+                <p><b>Total:</b> ${pedido.subTotal}</p>
+                <hr className="solid-divider" />
+                <h2>Productos:</h2>
+                {pedido.productos.map((producto, index) => (
+                    <div key={index} className='producto-detalle'>
+                        <p><b>Producto:</b> {producto.nombre}</p>
+                        <p><b>Cantidad:</b> {producto.cantItems}</p>
+                        <p><b>Precio:</b> ${producto.precioCombo}</p>
+                        {index < pedido.productos.length - 1 && <hr className="dotted-divider" />}
                     </div>
-                </div>
-            ))}
-
+                ))}
+            </div>
+            <div className="container-buttons-modal-detalles">
+                <button className="button-modal cancel" onClick={closeDetailsModal}>Cerrar</button>
+            </div>
+        </div>
+    </div>
+))}
             {/* {isModalOpen && (
                 <div className="container-modal" id="modal">
                     <div className="div-modal">
