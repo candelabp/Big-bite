b7069e1a5e8d84b17e28ae82668f23577af8fdfd
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2024-11-07 10:45:28 -0300

    boton home nav admin anda

--- src/bigBite/components/NavbarAdmin.jsx
+++ src/bigBite/components/NavbarAdmin.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import '../css/navbarAdmin.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logoNegro from '../assets/logoNegro.png'
+
 const NavbarAdmin = () =>{
+
+  const navigate=useNavigate();
+  const handleImageClick=()=>{
+    navigate("/")
+  }
+  
   return(
     <div>
       <header className="header">
-      <img className="logo-admin" src={logoNegro} alt="Logo-admin" />       
+      <img className="logo-admin" src={logoNegro} alt="Logo-admin" onClick={handleImageClick} />       
         <Link className="tituloLinkAdmin" to="/AdminPpal">Inicio</Link>
         <Link className="tituloLinkAdmin" to="/AdminProductos">Productos</Link>
         <Link className="tituloLinkAdmin" to="/Administradores">Administradores</Link>
