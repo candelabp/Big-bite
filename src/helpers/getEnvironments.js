accf95bfab71b2dcf5ca9ac2fc21114620451a6f
Candela Puerta <109047221+candelabp@users.noreply.github.com>
2025-01-31 22:42:43 -0300

    anda?

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,9 +1,11 @@
-export const getEnvironments = () =>{
-
-    import.meta.env;
-    
-    return{
-        ...import.meta.env
-    }
-
-}
+export const getEnvironments = () => {
+    return {
+      VITE_APIKEY: import.meta.env.VITE_APIKEY,
+      VITE_AUTHDOMAIN: import.meta.env.VITE_AUTHDOMAIN,
+      VITE_PROJECTID: import.meta.env.VITE_PROJECTID,
+      VITE_STORAGEBUCKET: import.meta.env.VITE_STORAGEBUCKET,
+      VITE_MESSAGINGSENDERID: import.meta.env.VITE_MESSAGINGSENDERID,
+      VITE_APPID: import.meta.env.VITE_APPID,
+    };
+  };
+  
