8026a54b686ee4bcb20f42cf8056ee217073e9fb
Alvaro Rubina <alvarorubina132@gmail.com>
2025-02-01 16:06:53 -0300

    Eliminado comentario innecesario

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,5 +1,4 @@
 export const getEnvironments = () => {
-    console.log(import.meta.env); // Agrega esta línea para verificar las variables de entorno
     return {
         ...import.meta.env
     };
